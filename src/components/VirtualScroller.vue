639d20c2b60799ac83ac99c59977270a42bfcea7
Linzdigr <fer.navy13@gmail.com>
2017-10-19 22:43:54 +0200

    Fix specific case, changing compute on height container #7

--- src/components/VirtualScroller.vue
+++ src/components/VirtualScroller.vue
@@ -239,7 +239,7 @@ export default {
 
         if (itemHeight !== null) {
           // Fixed height mode
-          offsetTop = (~~(l / itemsByRow)) * this.itemHeight
+          offsetTop = Math.ceil(startIndex / itemsByRow) * this.itemHeight
           containerHeight = Math.ceil(l / itemsByRow) * this.itemHeight
         }
 
