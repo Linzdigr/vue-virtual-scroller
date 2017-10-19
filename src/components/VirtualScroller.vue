040b28c638e2c6e70c1a0a6c1eb633a7fcc5242b
Linzdigr <fer.navy13@gmail.com>
2017-10-19 20:36:48 +0200

    Fix specific case, value not rounded correctly #2

--- src/components/VirtualScroller.vue
+++ src/components/VirtualScroller.vue
@@ -240,7 +240,7 @@ export default {
         if (itemHeight !== null) {
           // Fixed height mode
           offsetTop = startIndex * itemHeight
-          containerHeight = l * itemHeight
+          containerHeight = (l + (itemsByRow - (l % itemsByRow))) * itemHeight
         }
 
         if (force || startIndex !== this._startIndex || endIndex !== this._endIndex || l !== this._length) {
