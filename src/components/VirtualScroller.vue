a53e29a7b901eae55056e844dcedd06e0091bafa
Linzdigr <fer.navy13@gmail.com>
2017-11-20 21:12:00 +0100

    Bug fix on odd itemsByRow #4

--- src/components/VirtualScroller.vue
+++ src/components/VirtualScroller.vue
@@ -182,7 +182,7 @@ export default {
       if (this.itemWidth) {
         itemsByRow = ~~(this.$refs.vscrollContainer.getBoundingClientRect().width / this.itemWidth)
       }
-      const itemHeight = this.itemHeight / itemsByRow
+      const itemHeight = Math.floor(this.itemHeight / itemsByRow)
       let containerHeight, offsetTop
       if (scroll) {
         let startIndex = -1
@@ -226,7 +226,7 @@ export default {
           const buffer = this.buffer * (itemsByRow)
 
           startIndex = ~~(scroll.top / itemHeight) - buffer
-          startIndex -= ~~(startIndex % itemsByRow)
+          startIndex -= startIndex % itemsByRow
           endIndex = Math.ceil(scroll.bottom / itemHeight) + buffer
         }
 
@@ -239,7 +239,7 @@ export default {
 
         if (itemHeight !== null) {
           // Fixed height mode
-          offsetTop = ~~(startIndex / itemsByRow) * this.itemHeight
+          offsetTop = Math.ceil(startIndex / itemsByRow) * this.itemHeight
           containerHeight = Math.ceil(l / itemsByRow) * this.itemHeight
         }
 
