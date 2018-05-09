e0f14b54fe2488e517451e4c462bc3034641fa1a
root <root@donotreply@neko-san.fr>
2018-05-09 21:36:50 +0200

    Bug fix on incorrect containerHeight computation

--- src/components/VirtualScroller.vue
+++ src/components/VirtualScroller.vue
@@ -240,7 +240,7 @@ export default {
         if (itemHeight !== null) {
           // Fixed height mode
           offsetTop = ~~(startIndex / itemsByRow * this.itemHeight)
-          containerHeight = Math.ceil(l / itemsByRow * this.itemHeight)
+          containerHeight = Math.ceil(l / itemsByRow) * this.itemHeight
         }
 
         if (force || startIndex !== this._startIndex || endIndex !== this._endIndex || l !== this._length) {
